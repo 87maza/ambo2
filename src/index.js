cea484e93cbe61bd7959f4ba34ca63aa304839a5
87maza <87maza@gmail.com>
2016-09-25 09:37:27 -0400

    testing app

--- src/index.js
+++ src/index.js
@@ -3,4 +3,36 @@ import { render } from 'react-dom'
 import App from './components/app'
 import './styles/app.scss'
 
+
+
+
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, Link, browserHistory } from 'react-router'
+
+
+
+const App = () => {
+    return(
+        <div className="form">
+            <div id="signup">
+hello
+            </div>
+
+        </div>
+    )
+};
 render(<App/>, document.getElementById('main'))
+// ReactDOM.render((
+//     <Router history={browserHistory}>
+//         <Route path="/" component={App} />
+//         <Route path="/sign-in" component={SignUp} />
+//         <Route path="/passenger" component={Passenger}/>
+//         <Route path="/emergency" component={Emergency}/>
+//         <Route path="/modal" component={Modal} />
+//
+//     </Router>
+// ), document.querySelector('.app'));
